fde46ff53c2c4158d8c2bdd3af4b958b225c55cf
shannon oliver <shannon-alexis@hotmail.com>
2023-11-28 20:19:26 +0800

    ready to start adding other features now

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class NewsStory(models.Model):
+    # class Meta:
+    #     order_by = '-pub_date'
+
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
     pub_date = models.DateTimeField()
     content = models.TextField()
